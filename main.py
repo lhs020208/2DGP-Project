3700d344b4eb8afbbb1ecd53d4ffad56ea1a772b
lhs020208 <92709240+lhs020208@users.noreply.github.com>
2024-10-24 20:00:57 +0900

    kfm 애니메이션 완성

--- main.py
+++ main.py
@@ -106,6 +106,102 @@ class Kamijo:
         else:
             self.image.clip_composite_draw(self.framex * 140, self.framey * 140, 140, 140, 0, 'h', self.x, self.y, 150, 150)
 
+class KFM:
+    image = None
+    state = 'standing'
+    def __init__(self):
+        self.x = 450
+        self.y = 120
+        self.frame_step = 0
+        self.framex = 0
+        self.framey = 0
+        self.direct = 1
+
+    def find_frame_position(self, frame_step, w, h, max_frame):
+        frame_index = frame_step
+
+        self.framex = frame_index % w
+        self.framey = frame_index // w
+
+        total_rows = (max_frame + w - 1) // w  # Total rows in the sheet
+        self.framey = total_rows - 1 - self.framey
+
+        return self.framex, self.framey
+
+    def update(self):
+        if self.state == 'standing':
+            self.image = load_image('kfm_sheet/kfm_stand.png')
+            self.framex, self.framey = self.find_frame_position(self.frame_step, 6, 1, 6)
+            self.frame_step += 1
+            if self.frame_step == 6:
+                self.frame_step = 0
+        elif self.state == 'walk':
+            self.image = load_image('kfm_sheet/kfm_walk.png')
+            self.framex,self.framey = self.find_frame_position(self.frame_step,4,2,8 )
+            self.frame_step += 1
+            if self.frame_step == 8:
+                self.frame_step = 0
+        elif self.state == 'run':
+            self.image = load_image('kfm_sheet/kfm_run.png')
+            self.framex, self.framey = self.find_frame_position(self.frame_step, 6, 1, 6)
+            self.frame_step += 1
+            if self.frame_step == 6:
+                self.frame_step = 0
+        elif self.state == 'jump':
+            self.image = load_image('kfm_sheet/kfm_jump.png')
+            self.framex, self.framey = self.find_frame_position(self.frame_step, 5, 1, 5)
+            self.frame_step += 1
+            if self.frame_step == 5:
+                self.frame_step = 4
+        elif self.state == 'double jump':
+            self.image = load_image('kfm_sheet/kfm_jump.png')
+            self.framex, self.framey = self.find_frame_position(self.frame_step, 5, 1, 5)
+            self.frame_step += 1
+            if self.frame_step == 5:
+                self.frame_step = 4
+        elif self.state == 'normal_attack':
+            self.image = load_image('kfm_sheet/kfm_normal_attack.png')
+            self.framex, self.framey = self.find_frame_position(self.frame_step, 6, 1, 6)
+            self.frame_step += 1
+            if self.frame_step == 6:
+                self.frame_step = 0
+                self.framex = 0
+                self.framey = 0
+                self.state = 'standing'
+        elif self.state == 'special_attack':
+            self.image = load_image('kfm_sheet/kfm_special_attack.png')
+            self.framex, self.framey = self.find_frame_position(self.frame_step, 6, 1, 6)
+            self.frame_step += 1
+            if self.frame_step == 6:
+                self.frame_step = 0
+                self.framex = 0
+                self.framey = 0
+                self.state = 'standing'
+        elif self.state == 'heat':
+            self.image = load_image('kfm_sheet/kfm_heat.png')
+            self.framex, self.framey = self.find_frame_position(self.frame_step, 5, 1, 5)
+            self.frame_step += 1
+            if self.frame_step == 5:
+                self.frame_step = 0
+                self.framex = 0
+                self.framey = 0
+                self.state = 'standing'
+        elif self.state == 'thrown':
+            self.image = load_image('kfm_sheet/kfm_thrown.png')
+            self.framex, self.framey = self.find_frame_position(self.frame_step, 5, 1, 5)
+            self.frame_step += 1
+            if self.frame_step == 5:
+                self.frame_step = 3
+
+
+    def draw(self):
+        global Player_x, Player_y
+        offset_x = Player_x - 400
+        offset_y = Player_y - 120
+        if self.direct == 1:
+            self.image.clip_draw(self.framex * 140, self.framey * 140, 140, 140, self.x- offset_x, self.y- offset_y,150, 150)
+        else:
+            self.image.clip_composite_draw(self.framex * 140, self.framey * 140, 140, 140, 0, 'h', self.x- offset_x, self.y- offset_y, 150, 150)
 
 
 class Grass:
@@ -148,6 +244,7 @@ class Sky_Grass:
 def handle_events():
     global running, Player_x, Player_y
     global player
+    global enemy
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -221,6 +318,7 @@ def reset_world():
     global grass
     global sky_grass
     global player
+    global enemy
     global world
 
     running = True
@@ -234,6 +332,10 @@ def reset_world():
 
     player = Kamijo()
     world.append(player)
+
+    enemy = [KFM() for i in range(2)]
+    enemy[1].x -= 100
+    world += enemy
 
 def update_world():
     for o in world:
